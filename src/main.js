c00b50f57fd7a29ca1eafdfbd22d3ca162703ce1
darlaevans2000 <darlaevans2000@gmail.com>
2021-03-31 19:10:17 -0600

    Add function to random button and refactor previous randomBook function

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
 // Create variables targetting the relevant DOM elements here 👇
-var updateCoverImg = document.querySelector(".cover-image");
-var updateTitle = document.querySelector(".cover-title");
-var updateDescriptor1 = document.querySelector(".tagline-1");
-var updateDescriptor2 = document.querySelector(".tagline-2");
+var coverImg = document.querySelector(".cover-image");
+var title = document.querySelector(".cover-title");
+var descriptor1 = document.querySelector(".tagline-1");
+var descriptor2 = document.querySelector(".tagline-2");
+var randomButton = document.querySelector(".random-cover-button");
 
 
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+var currentCover = {};
+
+
 
 // Add your event listeners here 👇
 window.addEventListener("load", randomBook);
+randomButton.addEventListener("click", randomBook);
 
 // Create your event handlers and other functions here 👇
 
@@ -23,13 +27,14 @@ function getRandomIndex(array) {
 }
 
 function randomBook() {
-  var randomCoverImage = covers[getRandomIndex(covers)];
-  var randomTitle = titles[getRandomIndex(titles)];
-  var randomDesc1 = descriptors[getRandomIndex(descriptors)];
-  var randomDesc2 = descriptors[getRandomIndex(descriptors)];
+  coverImg.src = covers[getRandomIndex(covers)];
+  title.innerText = titles[getRandomIndex(titles)];
+  descriptor1.innerText = descriptors[getRandomIndex(descriptors)];
+  descriptor2.innerText = descriptors[getRandomIndex(descriptors)];
 
-  updateCoverImg.src = randomCoverImage;
-  updateTitle.innerText = randomTitle;
-  updateDescriptor1.innerText = randomDesc1;
-  updateDescriptor2.innerText = randomDesc2;
+  // updateCoverImg.src = randomCoverImage;
+  // updateTitle.innerText = randomTitle;
+  //  updateDescriptor1.innerText = randomDesc1;
+  // updateDescriptor2.innerText = randomDesc2;
+  currentCover = new Cover(coverImg.src, title.innerText, descriptor1.innerText, descriptor2.innerText);
 }
